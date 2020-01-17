1f48602f4c47f427cdb302520c656e7e6581a9ad
Ilya Azin <martis.azin@gmail.com>
2020-01-17 17:21:09 +0300

    add design

--- src/components/todo-header/todo-header.tsx
+++ src/components/todo-header/todo-header.tsx
@@ -4,8 +4,12 @@ import './todo-header.scss'
 const TodoHeader: React.FC = () => {
     return (
         <div className="todo-header">
-            <h1>Список задач</h1>
-            <button>Добавить</button>
+            <div className="todo-header__title">
+                <h1>Список задач</h1>
+            </div>
+            <div className="todo-header__action">
+                <button className="btn btn_success btn_alt-color btn_wide">Добавить</button>
+            </div>
         </div>
     )
 }
--- src/components/todo-item/todo-item.tsx
+++ src/components/todo-item/todo-item.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { ITask } from '../../interfaces';
 import './todo-item.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 interface Config {
     task: ITask;
@@ -22,8 +24,8 @@ const TodoItem: React.FC<Config> = ({ task }) => {
             <div className="cell todo-item__id">{ task.id} </div>
             <div className="cell todo-item__title">{ task.title} </div>
             <div className="cell todo-item__actions">
-                <button>E</button>
-                <button>&times;</button>
+                <button className="btn btn_success"><FontAwesomeIcon icon={faPen} className="fa-icon"/></button>
+                <button className="btn btn_danger"><FontAwesomeIcon icon={faTrashAlt} className="fa-icon"/></button>
             </div>
         </li>
     )
