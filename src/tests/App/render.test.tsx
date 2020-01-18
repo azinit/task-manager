1f9899797e652a7a4d4204d7c51020bf5c6b2b36
Ilya Azin <martis.azin@gmail.com>
2020-01-18 18:18:53 +0300

    final coverage

--- src/tests/App/render.test.tsx
+++ src/tests/App/render.test.tsx
@@ -3,8 +3,9 @@ import { render } from '@testing-library/react';
 import App from '../../client/App/App';
 import List from '../../client/pages/List';
 import Edit from '../../client/pages/Edit';
-import { Route, MemoryRouter, Redirect, Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { Route, MemoryRouter, Redirect, Router, useLocation, useRouteMatch } from 'react-router-dom';
+import { match } from 'react-router';
+import { createMemoryHistory, createLocation } from 'history';
 
 interface TextTest {
     caption: string;
@@ -12,34 +13,16 @@ interface TextTest {
 }
 
 describe('Page: List', () => {
-    const page = <List/>
+    const page = <List />
     const tests: TextTest[] = [
-        {caption: 'rendering main title', text: /Список задач/i},
-        {caption: 'rendering add button', text: /Добавить/i},
-        {caption: 'rendering footer', text: /© 2019 АО "Мегаполис"/i},
+        { caption: 'rendering main title', text: /Список задач/i },
+        { caption: 'rendering add button', text: /Добавить/i },
+        { caption: 'rendering footer', text: /© 2019 АО "Мегаполис"/i },
     ]
-    tests.forEach(({caption, text}) => {
+    tests.forEach(({ caption, text }) => {
         testContainText(caption, text, page);
     });
 })
-
-/*describe('Page: Edit', () => {
-    const history = createMemoryHistory
-    const page = (
-        <Router history={history}>
-            <App />
-        </Router>
-    )
-
-    const tests: TextTest[] = [
-        {caption: 'rendering remove button', text: /Удалить/i},
-        // {caption: 'rendering task id', text: /Задание №806/i},
-        // {caption: 'rendering task title', text: /[НЕ УДАЛЯТЬ] Задача для unit-тестов"/i},
-    ]
-    tests.forEach(({caption, text}) => {
-        testContainText(caption, text, page);
-    });
-})*/
 
 function testContainText(caption: string, text: RegExp, component: JSX.Element) {
     test(caption, () => {
