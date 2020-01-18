26635c6bc88b33108022036c3d8cdb2870e55dbd
Ilya Azin <martis.azin@gmail.com>
2020-01-18 13:53:08 +0300

    convert fetch to lib

--- src/server/fetch.ts
+++ src/server/fetch.ts
@@ -1,68 +1,18 @@
-import { ITask } from "../interfaces";
+import Fetch from './_fetch/fetch';
 
-const Urls = {
-    list: 'https://test.megapolis-it.ru/api/list',
-    add: 'https://test.megapolis-it.ru/api/list',
-    remove(id: number) {return `https://test.megapolis-it.ru/api/list/${id}`} ,
-    edit(id: number) {return `https://test.megapolis-it.ru/api/list/${id}`} ,
-}
-const Fetch = {
-    list() {
-        return fetch(Urls.list);
-    },
-    add(title: string) {
-        return fetch(Urls.add, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                title
-            }),
-        })
-    },
-    remove(id: number) {
-        return fetch(Urls.remove(id), {
-            method: 'DELETE',
-        });
-    },
-    edit(task: ITask) {
-        return fetch(Urls.edit(task.id), {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                title: task.title
-            }),
-        });
-    }
-}
+/**
+ * Библиотека по получению данных с сервера
+ * @library server/fetch
+ * @includes interfaces server/_fetch/interfaces
+ * @includes Fetch server/_fetch/fetch
+ * @author Азин И.А.
+ */
 
 export function onRemoveError() {
     console.error(`Объект не найден.
     Но обычный клиент не сможет добиться такой ситуации, т.к. для этого придется удалить несуществующее задание :)
     `);
 }
-export interface BaseResponse {
-    success: boolean;
-    error: string;
-}
 
-export interface AddResponse extends BaseResponse {
-    id: number
-}
-
-export interface ListResponse extends BaseResponse {
-    data: ITask[],
-    length: number,
-}
-
-export interface RemoveResponse extends BaseResponse {
-}
-
-export interface CallbackResponse {
-    (response: BaseResponse): void;
-}
-
-export default Fetch;
+export {BaseResponse, AddResponse, ListResponse, RemoveResponse, CallbackResponse} from './_fetch/interfaces';
+export default Fetch
