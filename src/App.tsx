492d3930b7f9c82f0d7161a89d125501ac833e49
Ilya Azin <martis.azin@gmail.com>
2020-01-18 12:12:27 +0300

    remove odd

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,8 @@ import './App.scss';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import List from './pages/List';
 import Edit from './pages/Edit';
-import { ITask } from './interfaces';
 
 const App: React.FC = () => {
-  const [tasks, setTasks] = React.useState<ITask[]>([
-    { id: 0, title: 'Завершить тестовое' },
-    { id: 1, title: 'Проверить MR' },
-    { id: 2, title: 'Поправить автодоку' },
-  ])
-
-  function add(title: string) {
-    setTasks([...tasks, {
-        title,
-        id: tasks.length,
-    }])
-  }
-
-  function remove(id: number) {
-      setTasks(tasks.filter(task => task.id !== id));
-  }
-
-  function edit(editedTask: ITask) {
-      setTasks(tasks.map(task => {
-          if (task.id == editedTask.id) {
-              task.title = editedTask.title;
-          }
-          return task;
-      }))
-  }
-
   return (
     <BrowserRouter>
       <div className="container">
