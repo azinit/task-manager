b71398907a9f07d7d0d7937de3205df2b04ff7f2
Ilya Azin <martis.azin@gmail.com>
2020-01-19 10:03:07 +0300

    Updates

---
+++ precache-manifest.3824cbba470e5b2250cfbd649dce11ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0383e5b728a8889f1a6dd512103d03",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "3c2c78d852b86cfbeda0",
+    "url": "/task-manager/static/css/4.4ddad681.chunk.css"
+  },
+  {
+    "revision": "d3eae398ee090a31ef15",
+    "url": "/task-manager/static/css/5.2b239dbf.chunk.css"
+  },
+  {
+    "revision": "9155bcafc1b05626e8ff",
+    "url": "/task-manager/static/css/6.8d861f8b.chunk.css"
+  },
+  {
+    "revision": "e7f156c7da0299677ccc",
+    "url": "/task-manager/static/css/main.5fc08fcb.chunk.css"
+  },
+  {
+    "revision": "1f863e080ab4834e86dc",
+    "url": "/task-manager/static/js/0.32bbdc0e.chunk.js"
+  },
+  {
+    "revision": "1cfab7874f44c7c41683",
+    "url": "/task-manager/static/js/3.c6dab46f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/task-manager/static/js/3.c6dab46f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c2c78d852b86cfbeda0",
+    "url": "/task-manager/static/js/4.0b7bac0b.chunk.js"
+  },
+  {
+    "revision": "d3eae398ee090a31ef15",
+    "url": "/task-manager/static/js/5.b2bed8e2.chunk.js"
+  },
+  {
+    "revision": "9155bcafc1b05626e8ff",
+    "url": "/task-manager/static/js/6.3d9a1fef.chunk.js"
+  },
+  {
+    "revision": "e7f156c7da0299677ccc",
+    "url": "/task-manager/static/js/main.e8d6b2c4.chunk.js"
+  },
+  {
+    "revision": "8e7fa9c0f23427bb078e",
+    "url": "/task-manager/static/js/runtime-main.109572f0.js"
+  }
+]);
